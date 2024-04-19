591e7e12d1bc524d0696f9c8f75f7c0fc372a8c7
igl-myrick <igl.myrick@gmail.com>
2024-04-19 15:10:48 -0700

    add test for equilateral triangle check

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -23,4 +23,9 @@ describe('Triangle', () => {
     const isosTriangle = new Triangle(5,5,7);
     expect(isosTriangle.checkType()).toEqual("isosceles triangle");
   })
+
+  test('should correctly determine whether three lengths make an equilateral triangle', () => {
+    const equiTriangle = new Triangle(5,5,5);
+    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
 });
